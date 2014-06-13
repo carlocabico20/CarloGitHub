3eb0a8230bef89eb5c19ec402400749dbf6a45ef
CarloCabico <carlocabico20@yahoo.com>
2014-06-13 13:33:49 +0800

    tool3 commit

---
+++ ButtonUserControl/ButtonUserControl/UserControl1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ButtonUserControl
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTxt = new System.Windows.Forms.TextBox();
+            this.SwAddrTxt = new System.Windows.Forms.TextBox();
+            this.DoAddrTxt = new System.Windows.Forms.TextBox();
+            this.AnsAddrTxt = new System.Windows.Forms.TextBox();
+            this.SwStatBtn = new System.Windows.Forms.Button();
+            this.DoStatBtn = new System.Windows.Forms.Button();
+            this.AnsStatBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnTxt
+            // 
+            this.BtnTxt.Enabled = false;
+            this.BtnTxt.Location = new System.Drawing.Point(6, 8);
+            this.BtnTxt.Name = "BtnTxt";
+            this.BtnTxt.Size = new System.Drawing.Size(82, 20);
+            this.BtnTxt.TabIndex = 0;
+            // 
+            // SwAddrTxt
+            // 
+            this.SwAddrTxt.Enabled = false;
+            this.SwAddrTxt.Location = new System.Drawing.Point(94, 8);
+            this.SwAddrTxt.Name = "SwAddrTxt";
+            this.SwAddrTxt.Size = new System.Drawing.Size(100, 20);
+            this.SwAddrTxt.TabIndex = 1;
+            // 
+            // DoAddrTxt
+            // 
+            this.DoAddrTxt.Enabled = false;
+            this.DoAddrTxt.Location = new System.Drawing.Point(281, 8);
+            this.DoAddrTxt.Name = "DoAddrTxt";
+            this.DoAddrTxt.Size = new System.Drawing.Size(100, 20);
+            this.DoAddrTxt.TabIndex = 3;
+            // 
+            // AnsAddrTxt
+            // 
+            this.AnsAddrTxt.Enabled = false;
+            this.AnsAddrTxt.Location = new System.Drawing.Point(468, 8);
+            this.AnsAddrTxt.Name = "AnsAddrTxt";
+            this.AnsAddrTxt.Size = new System.Drawing.Size(100, 20);
+            this.AnsAddrTxt.TabIndex = 5;
+            // 
+            // SwStatBtn
+            // 
+            this.SwStatBtn.Location = new System.Drawing.Point(200, 7);
+            this.SwStatBtn.Name = "SwStatBtn";
+            this.SwStatBtn.Size = new System.Drawing.Size(75, 23);
+            this.SwStatBtn.TabIndex = 6;
+            this.SwStatBtn.Text = "On/Off";
+            this.SwStatBtn.UseVisualStyleBackColor = true;
+            this.SwStatBtn.Click += new System.EventHandler(this.SwStatBtn_Click);
+            // 
+            // DoStatBtn
+            // 
+            this.DoStatBtn.Location = new System.Drawing.Point(387, 7);
+            this.DoStatBtn.Name = "DoStatBtn";
+            this.DoStatBtn.Size = new System.Drawing.Size(75, 23);
+            this.DoStatBtn.TabIndex = 7;
+            this.DoStatBtn.Text = "On/Off";
+            this.DoStatBtn.UseVisualStyleBackColor = true;
+            this.DoStatBtn.Click += new System.EventHandler(this.DoStatBtn_Click);
+            // 
+            // AnsStatBtn
+            // 
+            this.AnsStatBtn.Location = new System.Drawing.Point(574, 7);
+            this.AnsStatBtn.Name = "AnsStatBtn";
+            this.AnsStatBtn.Size = new System.Drawing.Size(75, 23);
+            this.AnsStatBtn.TabIndex = 8;
+            this.AnsStatBtn.Text = "On/Off";
+            this.AnsStatBtn.UseVisualStyleBackColor = true;
+            this.AnsStatBtn.Click += new System.EventHandler(this.AnsStatBtn_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.AnsStatBtn);
+            this.Controls.Add(this.DoStatBtn);
+            this.Controls.Add(this.SwStatBtn);
+            this.Controls.Add(this.AnsAddrTxt);
+            this.Controls.Add(this.DoAddrTxt);
+            this.Controls.Add(this.SwAddrTxt);
+            this.Controls.Add(this.BtnTxt);
+            this.Enabled = false;
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(654, 36);           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BtnTxt;
+        private System.Windows.Forms.TextBox SwAddrTxt;
+        private System.Windows.Forms.TextBox DoAddrTxt;
+        private System.Windows.Forms.TextBox AnsAddrTxt;
+        private System.Windows.Forms.Button SwStatBtn;
+        private System.Windows.Forms.Button DoStatBtn;
+        private System.Windows.Forms.Button AnsStatBtn;
+    }
+}
---
+++ ButtonUserControl/ButtonUserControl/UserControl1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel.Design;
+
+namespace ButtonUserControl
+{
+     [Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(IDesigner))]
+    public partial class UserControl1: UserControl
+    {
+         
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        
+        public Button SwButton
+        {
+            get
+            {
+                return SwStatBtn;
+            }
+            set
+            {
+                SwStatBtn = value;
+            }
+
+        }
+
+        public Button DoButton
+        {
+            get
+            {
+                return DoStatBtn;
+            }
+            set
+            {
+                DoStatBtn = value;
+            }
+
+        }
+
+        public Button AnsButton
+        {
+            get
+            {
+                return AnsStatBtn;
+            }
+            set
+            {
+                AnsStatBtn = value;
+            }
+
+        }
+        
+        [Browsable(true)]
+        [Description("Button Text")]
+        [Category("Appearance")]
+        public string ButtonTextbox
+        {
+            get
+            {
+                return this.BtnTxt.Text;
+            }
+            set
+            {
+                this.BtnTxt.Text = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Switch Signal Address Text")]
+        [Category("Appearance")]
+        public string SwAddrTextbox
+        {
+            get
+            {
+                return this.SwAddrTxt.Text;
+            }
+            set
+            {
+                this.SwAddrTxt.Text = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Digital Output Signal Address Text")]
+        [Category("Appearance")]
+        public string DoAddrTextbox
+        {
+            get
+            {
+                return this.DoAddrTxt.Text;
+            }
+            set
+            {
+                this.DoAddrTxt.Text = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Answer Signal Address Text")]
+        [Category("Appearance")]
+        public string AnsAddrTextbox
+        {
+            get
+            {
+                return this.AnsAddrTxt.Text;
+            }
+            set
+            {
+                this.AnsAddrTxt.Text = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Switch Signal Button Text")]
+        [Category("Appearance")]
+        public string SwBtnTextbox
+        {
+            get
+            {
+                return this.SwStatBtn.Text;
+            }
+            set
+            {
+                this.SwStatBtn.Text = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Digital Output Signal Button Text")]
+        [Category("Appearance")]
+        public string DoBtnTextbox
+        {
+            get
+            {
+                return this.DoStatBtn.Text;
+            }
+            set
+            {
+                this.DoStatBtn.Text = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Answer Signal Button Text")]
+        [Category("Appearance")]
+        public string AnsBtnTextbox
+        {
+            get
+            {
+                return this.AnsStatBtn.Text;
+            }
+            set
+            {
+                this.AnsStatBtn.Text = value;
+            }
+        }
+
+
+        private void SwStatBtn_Click(object sender, EventArgs e)
+        {
+            var parent = this.Parent as MainUserControl.UserControl1;
+            int value;
+            if (SwAddrTxt.Text.Contains("/"))
+            {
+                if (SwStatBtn.Text == "Off")
+                {
+                    value = 0;
+                    parent.BtnUcs[0].SwStatBtn.Text = "Off";
+                }
+                else
+                {
+                    value = -1;
+                    parent.BtnUcs[0].SwStatBtn.Text = "On";
+                }                
+                parent.OpcOnOff(SwAddrTxt.Text, value, SwStatBtn);
+               
+            }
+            else
+            {
+                if (SwStatBtn.Text == "On")
+                {
+                    value = 0;
+                    parent.BtnUcs[1].SwStatBtn.Text = "On";
+                }
+                else
+                {
+                    value = -1;
+                    parent.BtnUcs[1].SwStatBtn.Text = "Off";
+                }
+                parent.OpcOnOff(SwAddrTxt.Text, value, SwStatBtn);
+           }
+        }
+
+        private void DoStatBtn_Click(object sender, EventArgs e)
+        {
+            var parent = this.Parent as MainUserControl.UserControl1;
+            int value;
+            if (DoAddrTxt.Text.Contains("/"))
+            {
+                if (DoStatBtn.Text == "Off")
+                {
+                    value = 0;
+                    parent.BtnUcs[0].DoStatBtn.Text = "Off";
+                }
+                else
+                {
+                    value = -1;
+                    parent.BtnUcs[0].DoStatBtn.Text = "On";
+                }
+                parent.OpcOnOff(DoAddrTxt.Text, value, DoStatBtn);
+            }
+            else
+            {
+                if (DoStatBtn.Text == "On")
+                {
+                    value = 0;
+                    parent.BtnUcs[1].DoStatBtn.Text = "On";
+                }
+                else
+                {
+                    value = -1;
+                    parent.BtnUcs[1].DoStatBtn.Text = "Off";
+                }
+            }
+            parent.OpcOnOff(DoAddrTxt.Text, value, DoStatBtn);
+        }
+
+        private void AnsStatBtn_Click(object sender, EventArgs e)
+        {
+            var parent = this.Parent as MainUserControl.UserControl1;
+            int value;
+            if (AnsAddrTxt.Text.Contains("/"))
+            {
+                if (AnsStatBtn.Text == "Off")
+                {
+                    value = 0;
+                    parent.BtnUcs[0].AnsStatBtn.Text = "Off";
+                }
+                else
+                {
+                    value = -1;
+                    parent.BtnUcs[0].AnsStatBtn.Text = "On";
+                }
+                parent.OpcOnOff(AnsAddrTxt.Text, value, AnsStatBtn);
+            }
+            else
+            {
+                if (AnsStatBtn.Text == "On")
+                {
+                    value = 0;
+                    parent.BtnUcs[1].AnsStatBtn.Text = "On";
+                }
+                else
+                {
+                    value = -1;
+                    parent.BtnUcs[1].AnsStatBtn.Text = "Off";
+                }
+                parent.OpcOnOff(AnsAddrTxt.Text, value, AnsStatBtn);
+            }
+        }
+
+    }
+}
