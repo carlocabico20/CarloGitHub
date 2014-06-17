6aa76033af6df9f1db8e9fd5d3b37ed903a9ee7b
CarloCabico <carlocabico20@yahoo.com>
2014-06-17 14:02:23 +0800

    tool3 commit

---
+++ MainUserControl/MainUserControl/UserControl1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace MainUserControl
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TagTxt = new System.Windows.Forms.TextBox();
+            this.Instru1Txt = new System.Windows.Forms.TextBox();
+            this.Instru2Txt = new System.Windows.Forms.TextBox();
+            this.AmAddrTxt = new System.Windows.Forms.TextBox();
+            this.AmStatBtn = new System.Windows.Forms.Button();
+            this.OpAddrTxt = new System.Windows.Forms.TextBox();
+            this.OpAddrBtn = new System.Windows.Forms.Button();
+            this.OPStatText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Btn4Uc = new ButtonUserControl.UserControl1(opc);
+            this.Btn3Uc = new ButtonUserControl.UserControl1(opc);
+            this.Btn2Uc = new ButtonUserControl.UserControl1(opc);
+            this.Btn1Uc = new ButtonUserControl.UserControl1(opc);
+            this.SuspendLayout();
+            // 
+            // TagTxt
+            // 
+            this.TagTxt.Enabled = false;
+            this.TagTxt.Location = new System.Drawing.Point(6, 6);
+            this.TagTxt.Name = "TagTxt";
+            this.TagTxt.Size = new System.Drawing.Size(100, 20);
+            this.TagTxt.TabIndex = 1;
+            // 
+            // Instru1Txt
+            // 
+            this.Instru1Txt.Enabled = false;
+            this.Instru1Txt.Location = new System.Drawing.Point(112, 6);
+            this.Instru1Txt.Name = "Instru1Txt";
+            this.Instru1Txt.Size = new System.Drawing.Size(156, 20);
+            this.Instru1Txt.TabIndex = 2;
+            // 
+            // Instru2Txt
+            // 
+            this.Instru2Txt.Enabled = false;
+            this.Instru2Txt.Location = new System.Drawing.Point(274, 6);
+            this.Instru2Txt.Name = "Instru2Txt";
+            this.Instru2Txt.Size = new System.Drawing.Size(156, 20);
+            this.Instru2Txt.TabIndex = 3;
+            // 
+            // AmAddrTxt
+            // 
+            this.AmAddrTxt.Enabled = false;
+            this.AmAddrTxt.Location = new System.Drawing.Point(436, 6);
+            this.AmAddrTxt.Name = "AmAddrTxt";
+            this.AmAddrTxt.Size = new System.Drawing.Size(100, 20);
+            this.AmAddrTxt.TabIndex = 4;
+            // 
+            // AmStatBtn
+            // 
+            this.AmStatBtn.Location = new System.Drawing.Point(542, 5);
+            this.AmStatBtn.Name = "AmStatBtn";
+            this.AmStatBtn.Size = new System.Drawing.Size(75, 23);
+            this.AmStatBtn.TabIndex = 7;
+            this.AmStatBtn.Text = "On/Off";
+            this.AmStatBtn.UseVisualStyleBackColor = true;
+            this.AmStatBtn.Click += new System.EventHandler(this.AmStatBtn_Click);
+            // 
+            // OpAddrTxt
+            // 
+            this.OpAddrTxt.Enabled = false;
+            this.OpAddrTxt.Location = new System.Drawing.Point(623, 6);
+            this.OpAddrTxt.Name = "OpAddrTxt";
+            this.OpAddrTxt.Size = new System.Drawing.Size(100, 20);
+            this.OpAddrTxt.TabIndex = 8;
+            // 
+            // OpAddrBtn
+            // 
+            this.OpAddrBtn.Location = new System.Drawing.Point(729, 5);
+            this.OpAddrBtn.Name = "OpAddrBtn";
+            this.OpAddrBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpAddrBtn.TabIndex = 9;
+            this.OpAddrBtn.Text = "OP Button";
+            this.OpAddrBtn.UseVisualStyleBackColor = true;
+            this.OpAddrBtn.Click += new System.EventHandler(this.OpAddrBtn_Click);
+            // 
+            // OPStatText
+            // 
+            this.OPStatText.Location = new System.Drawing.Point(816, 6);
+            this.OPStatText.Name = "OPStatText";
+            this.OPStatText.Size = new System.Drawing.Size(100, 20);
+            this.OPStatText.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Button  Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(245, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "SW Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(347, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "SW Status";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(427, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "DO Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(533, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 17);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "DO Status";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(617, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 17);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "ANS Address";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(723, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 17);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "ANS Status";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(37, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Button 1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(37, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 17);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Button 2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(37, 147);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 17);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "Button 3";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(37, 185);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 17);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "Button 4";
+            // 
+            // Btn4Uc
+            // 
+            this.Btn4Uc.AnsAddrTextbox = "";
+            this.Btn4Uc.AnsBtnTextbox = "On/Off";
+            this.Btn4Uc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Btn4Uc.ButtonTextbox = "";
+            this.Btn4Uc.DoAddrTextbox = "";
+            this.Btn4Uc.DoBtnTextbox = "On/Off";
+            this.Btn4Uc.Enabled = false;
+            this.Btn4Uc.Location = new System.Drawing.Point(146, 174);
+            this.Btn4Uc.Name = "Btn4Uc";
+            this.Btn4Uc.Size = new System.Drawing.Size(658, 39);
+            this.Btn4Uc.SwAddrTextbox = "";
+            this.Btn4Uc.SwBtnTextbox = "On/Off";
+            this.Btn4Uc.TabIndex = 13;
+            // 
+            // Btn3Uc
+            // 
+            this.Btn3Uc.AnsAddrTextbox = "";
+            this.Btn3Uc.AnsBtnTextbox = "On/Off";
+            this.Btn3Uc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Btn3Uc.ButtonTextbox = "";
+            this.Btn3Uc.DoAddrTextbox = "";
+            this.Btn3Uc.DoBtnTextbox = "On/Off";
+            this.Btn3Uc.Enabled = false;
+            this.Btn3Uc.Location = new System.Drawing.Point(146, 136);
+            this.Btn3Uc.Name = "Btn3Uc";
+            this.Btn3Uc.Size = new System.Drawing.Size(658, 39);
+            this.Btn3Uc.SwAddrTextbox = "";
+            this.Btn3Uc.SwBtnTextbox = "On/Off";
+            this.Btn3Uc.TabIndex = 12;
+            // 
+            // Btn2Uc
+            // 
+            this.Btn2Uc.AnsAddrTextbox = "";
+            this.Btn2Uc.AnsBtnTextbox = "On/Off";
+            this.Btn2Uc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Btn2Uc.ButtonTextbox = "";
+            this.Btn2Uc.DoAddrTextbox = "";
+            this.Btn2Uc.DoBtnTextbox = "On/Off";
+            this.Btn2Uc.Enabled = false;
+            this.Btn2Uc.Location = new System.Drawing.Point(146, 99);
+            this.Btn2Uc.Name = "Btn2Uc";
+            this.Btn2Uc.Size = new System.Drawing.Size(658, 39);
+            this.Btn2Uc.SwAddrTextbox = "";
+            this.Btn2Uc.SwBtnTextbox = "On/Off";
+            this.Btn2Uc.TabIndex = 11;
+            // 
+            // Btn1Uc
+            // 
+            this.Btn1Uc.AnsAddrTextbox = "";
+            this.Btn1Uc.AnsBtnTextbox = "On/Off";
+            this.Btn1Uc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Btn1Uc.ButtonTextbox = "";
+            this.Btn1Uc.DoAddrTextbox = "";
+            this.Btn1Uc.DoBtnTextbox = "On/Off";
+            this.Btn1Uc.Enabled = false;
+            this.Btn1Uc.Location = new System.Drawing.Point(146, 61);
+            this.Btn1Uc.Name = "Btn1Uc";
+            this.Btn1Uc.Size = new System.Drawing.Size(658, 39);
+            this.Btn1Uc.SwAddrTextbox = "";
+            this.Btn1Uc.SwBtnTextbox = "On/Off";
+            this.Btn1Uc.TabIndex = 10;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OPStatText);
+            this.Controls.Add(this.Btn4Uc);
+            this.Controls.Add(this.Btn3Uc);
+            this.Controls.Add(this.Btn2Uc);
+            this.Controls.Add(this.Btn1Uc);
+            this.Controls.Add(this.OpAddrBtn);
+            this.Controls.Add(this.OpAddrTxt);
+            this.Controls.Add(this.AmStatBtn);
+            this.Controls.Add(this.AmAddrTxt);
+            this.Controls.Add(this.Instru2Txt);
+            this.Controls.Add(this.Instru1Txt);
+            this.Controls.Add(this.TagTxt);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(923, 219);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TagTxt;
+        private System.Windows.Forms.TextBox Instru1Txt;
+        private System.Windows.Forms.TextBox Instru2Txt;
+        private System.Windows.Forms.TextBox AmAddrTxt;
+        private System.Windows.Forms.Button AmStatBtn;
+        private System.Windows.Forms.TextBox OpAddrTxt;
+        private System.Windows.Forms.Button OpAddrBtn;
+        private ButtonUserControl.UserControl1 Btn1Uc;
+        private ButtonUserControl.UserControl1 Btn2Uc;
+        private ButtonUserControl.UserControl1 Btn4Uc;
+        private ButtonUserControl.UserControl1 Btn3Uc;
+        private System.Windows.Forms.TextBox OPStatText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+
+    }
+}
